6327ed715ea2907836086768a0280bdc12ba060e
Yoni vidal <yonivid@gmail.com>
2020-06-18 19:06:03 +0300

    New_Branch

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
-const num = document.getElementById("num");
-const btn = document.getElementById("btn");
+let num = document.getElementById("num");
+let btn = document.getElementById("btn");
 let sum = 0;
 
-btn.addEventListener(onclick, btnClick  =>{
-    for(index=0; index<=num; i++){
-        sum = sum + index;
+btn.addEventListener(onclick, btnClick  => {
+    for(index=0; index<=num.value; index++){
+        sum += index;
     }
+    return sum;
     
 })
-document.writeln(sum);
+
 
 
 
