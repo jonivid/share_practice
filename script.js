669943f60c006b315c338d6274cad25d70d82c77
Yoni vidal <yonivid@gmail.com>
2020-06-18 18:27:09 +0300

    share_practice

---
+++ script.js
@@ -0,0 +1,16 @@
+const num = document.getElementById("num");
+const btn = document.getElementById("btn");
+let sum = 0;
+
+btn.addEventListener(onclick, btnClick  =>{
+    for(index=0; index<=num; i++){
+        sum = sum + index;
+    }
+    
+})
+document.writeln(sum);
+
+
+
+  
+
